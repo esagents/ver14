172dc3bad33fbebff647e365e45d6ed4a72a4c85
esagents <vu.nguyenlt@esagents.com>
2021-03-12 14:49:22 +0000

    data_API

---
+++ data_API/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "data_API",
+
+    'summary': """data API app""",
+
+    'description': """nothing""",
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+    'sequence': 10,
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [],
+    # only loaded in demonstration mode
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
